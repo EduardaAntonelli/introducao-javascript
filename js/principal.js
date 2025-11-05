ccc816a60e5783d8fc7edb69f32b928f20f8e7f5
EduardaAntonelli <eduarda.antonelli.souza@escola.pr.gov.br>
2025-11-05 11:36:34 -0300

    ims

--- js/principal.js
+++ js/principal.js
@@ -1,4 +1,16 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
-/* IMC = massa/altura^2
+/* IMC = massa/altura^2  */
+var paciente = document.querySelector("primeiro-paciente");
+var tdPeso = document.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+var tdAltura = document.querySelector(".info-altura");
+var Altura = tdAltura.textContent;
+var imc = peso / (altura*altura);
+
+console.log(paciente); //tr
+console.log(tdPeso); // td que tem peso
+console.log(tdAltura); // td que tem altura
+console.log(peso); //valor do peso
+console.log(altura); //valor da altura
